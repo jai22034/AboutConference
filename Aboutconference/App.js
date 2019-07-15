937ea5f0ef6854e40274a3e855eb68e32b1a3524
j2itu <jai22034@gmail.com>
2019-07-15 13:48:59 +0530

    AboutConferences   24/06/2019

---
+++ Aboutconference/App.js
@@ -0,0 +1,42 @@
+
+import React, {Component} from 'react';
+import {Platform, StyleSheet, Text, View} from 'react-native';
+
+
+export default class App extends Component {
+  render() {
+    return (
+      <View style={{flex:1}}>
+        <View style={{flex:1,marginLeft:3}}>
+       <Text style={{fontSize:16}}>Globally, industry is going thorugh the most rapid and transformational shift driven by technological progress, that they've ever encountered NASSCOM has always had an ear to the ground, enabling, facilitating and driving the digital transformation story for India</Text>
+      </View>
+      <View style={{flex:.4,marginLeft:3}}>
+
+        </View>
+        <View style={{flex:1,marginLeft:3}}>
+        <Text style={{fontSize:16.7}}>The NASSCOM HR Summit focuses on the most crucial resource for this transformation-the humans. At the 16th annual edition of the event, witness HR in an all -new avatar -a deep dive into examples of transformation from across the world. We're calling it HRFlix;Living the 'Tomorrow of HR' Today
+
+        </Text>
+        </View>
+        <View style={{flex:.4,marginLeft:3}}>
+
+</View>
+<View style={{flex:.4,marginLeft:3}}>
+        <Text style={{fontSize:16.7}}>It's a binge watch of fascinating business stories of action that we promise, wioll be riveting and thought provoking.
+        </Text>
+        </View>
+        <View style={{flex:.4,marginLeft:3}}>
+
+</View>
+<View style={{flex:6.6,marginLeft:3}}>
+        <Text style={{fontSize:16.7}}>#StopTalkingDigital #StarBeingDigitl
+        </Text>
+        </View>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+
+});
